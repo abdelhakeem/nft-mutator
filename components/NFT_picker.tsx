c18971e4760455d13beccb68c1100cbed23d75bf
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2022-07-27 19:28:07 +0530

    skelton added for file structure

---
+++ components/NFT_picker.tsx
@@ -0,0 +1,14 @@
+////import styles from './../styles/Home.module.css'
+import type { NextPage } from 'next'
+import styles from './../styles/NFT_picker.module.css'
+
+const NFT_picker:NextPage = () => {
+
+    return (
+        <div className={styles.border}>
+           
+        </div>
+    );
+}
+
+export default NFT_picker
---
+++ pages/mutate.tsx
@@ -0,0 +1,13 @@
+import MidSection from '../components/nft_picker'
+
+const SecondPage = () => {
+  return (    
+    <div>
+    <MidSection />
+ 
+      
+    </div>
+  )
+}
+
+export default SecondPage
