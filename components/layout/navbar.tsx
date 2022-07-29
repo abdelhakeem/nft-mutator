f7f12ddc0881f504713086b795f1712c5916f40c
Tess99854 <stesnime@yahoo.com>
2022-07-29 16:40:16 +0100

    fix page content issues

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -9,28 +9,21 @@ const Navbar = () => {
     const {connected} = useWallet()
 
     return (
-        <div className="drawer w-full">
-            <input id="side-drawer" type="checkbox" className="drawer-toggle"/>
+                <div className="flex w-full justify-between h-20 pt-8 container mx-auto" style={{overflow: "visible"}}>
+                    <a className="relative w-1/3 md:w-[15%]">
+                        <Image src="/NFTMutator_logo.svg" layout="fill"></Image>
+                    </a>
 
-            <div className="flex drawer-content w-full justify-between h-20 pt-8 container mx-auto" style={{overflow: "visible"}}>
-                <a className="relative w-1/3 md:w-[15%]">
-                    <Image src="/NFTMutator_logo.svg" layout="fill"></Image>
-                </a>
+                    <div className="justify-center gap-8 items-center wallet-button-parent hidden md:flex">
+                        { connected ? <NavbarLink href="/mutate" text="mutate"></NavbarLink> : null}
+                        <ClusterPickerWeb/>
+                        <WalletMultiButton/>
+                    </div>
 
-                <div className="justify-center gap-8 items-center wallet-button-parent hidden md:flex">
-                    { connected ? <NavbarLink href="/mutate" text="mutate"></NavbarLink> : null}
-                    <ClusterPickerWeb/>
-                    <WalletMultiButton/>
+                    <label htmlFor="side-drawer" className="md:hidden relative w-6 cursor-pointer">
+                        <Image src="/menu.svg" layout="fill"></Image>
+                    </label>
                 </div>
-
-                <label htmlFor="side-drawer" className="md:hidden relative w-6 cursor-pointer">
-                    <Image src="/menu.svg" layout="fill"></Image>
-                </label>
-            </div>
-
-            <SideDrawer/>
-
-        </div>
     )
 }
 
