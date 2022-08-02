24e0b01e4107c29a467023c1d893ebc69e9f0571
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2022-08-02 14:09:14 +0200

    Revert "Hero section"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,7 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import HeroSection from "../components/layout/heroSection";
-import { useWalletModal } from "@solana/wallet-adapter-react-ui";
-import { useWallet } from "@solana/wallet-adapter-react";
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const router = useRouter();
-  const { publicKey } = useWallet()
-  const walletModal = useWalletModal();
-  const handleGetStarted = () => {
-    if (publicKey) {
-      //if user wallet is connected
-      router.push("/mutate");
-    } else {
-      //show connect wallet modal
-      walletModal.setVisible(true)
-    }
-  };
+  return <div></div>
+}
 
-  return (
-    <div>
-      <Head>
-        <title>NFT Mutator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <HeroSection onHandleGetStarted={handleGetStarted} />
-    </div>
-  );
-};
-
-export default Home;
+export default Home
