1a51281d99dcb7886333aed9d5f7e62ad81f1341
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2022-08-03 21:05:31 +0530

    nft-selection skelton and dimensions added

--- components/layout/nft_selection.tsx
+++ components/layout/nft_selection.tsx
@@ -1,3 +1,5 @@
+import styles from '../../styles/nft_selection.module.css';
+
 type nft_props = {
     url: string,
     name: string,
@@ -5,13 +7,38 @@ type nft_props = {
     info_data: string
 }
 
-let nftData: nft_props = { url: 'https://i.imgur.com/XyqQZQz.png', name: 'nft1', id: 'nft1', info_data: 'nft1' };
+let nftData: nft_props = { url: 'https://i.imgur.com/XyqQZQz.png', name: 'nft1', id: 'nft1#3245', info_data: 'nft1 info data in white' };
 
 const NFT_Selection = () => {
-    
+
     return (
-        <div >
-            Hello this is a nFT MURTATTE TEST
+        <div className={styles.parentDiv +" "+styles.columnParent + " " + styles.middle}>
+            <div className={styles.margin20}>
+                <h1><b>Selection</b></h1>
+            </div>
+            <div className={styles.blank}>
+            </div>
+            <div className={styles.contentDiv}>
+                <div className={styles.margin20}>
+                    <div className="avatar">
+                        <div className="w-24 rounded">
+                            <img src={nftData.url} />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.columnParent +" "+styles.margin20}>
+                    <div>
+                        <h2>{nftData.id}</h2>
+                    </div>
+                    <div>
+                        {nftData.name}
+                    </div>
+                    <div>
+                        {nftData.info_data}
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
