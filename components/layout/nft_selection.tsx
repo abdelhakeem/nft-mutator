bdd68bf16a6cdc566730fa9c9c55fedbca271d24
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2022-08-03 20:09:58 +0530

    router added and workflow done for the nft-selection

---
+++ components/layout/nft_selection.tsx
@@ -0,0 +1,19 @@
+type nft_props = {
+    url: string,
+    name: string,
+    id: string,
+    info_data: string
+}
+
+let nftData: nft_props = { url: 'https://i.imgur.com/XyqQZQz.png', name: 'nft1', id: 'nft1', info_data: 'nft1' };
+
+const NFT_Selection = () => {
+    
+    return (
+        <div >
+            Hello this is a nFT MURTATTE TEST
+        </div>
+    )
+}
+
+export default NFT_Selection
