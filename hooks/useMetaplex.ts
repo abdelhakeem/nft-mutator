b98e26e36b27fb56ee495357d80a0f7f0ef11429
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2022-08-04 13:30:08 +0200

    Fix useMetaplex hook return value

--- hooks/useMetaplex.ts
+++ hooks/useMetaplex.ts
@@ -13,20 +13,22 @@ const useMetaplex = () => {
   const wallet = useWallet();
   const cluster = useAppSelector(selectCluster);
 
-  return useMemo(() => {
-    wallet
-      ? Metaplex.make(connection)
-          .use(walletAdapterIdentity(wallet))
-          .use(
-            bundlrStorage({
-              address:
-                cluster === "devnet"
-                  ? "https://devnet.bundlr.network"
-                  : undefined,
-            })
-          )
-      : undefined;
-  }, [connection, wallet, cluster]);
+  return useMemo(
+    () =>
+      wallet
+        ? Metaplex.make(connection)
+            .use(walletAdapterIdentity(wallet))
+            .use(
+              bundlrStorage({
+                address:
+                  cluster === "devnet"
+                    ? "https://devnet.bundlr.network"
+                    : undefined,
+              })
+            )
+        : undefined,
+    [connection, wallet, cluster]
+  );
 };
 
 export default useMetaplex;
