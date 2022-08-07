e89aa7e0dfa499029174f46dd51fd3fbc2fa57b2
Rishabh Vasudevan <rishabhvasudevan19@gmail.com>
2022-08-07 20:18:30 +0530

    making the design for the second page

---
+++ components/layout/NFTFetched.tsx
@@ -0,0 +1,187 @@
+export default function NFTFetched() {
+    return (
+        <div>
+            <div className="n-f-t-fetched-n-f-t-fetched container m-auto">
+
+                <p className="n-f-t-fetched-your-nfts">Your NFTs&#160;</p>
+                <div className="n-f-t-fetched-rectangle9" />
+                <div className="intersect-intersect">
+                    <svg
+                        width="100%"
+                        height="100%"
+                        preserveAspectRatio="none"
+                        viewBox="0 0 150 166"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                    >
+                        <mask id="path-1-inside-1_1_40" fill="white">
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M10 0C4.47715 0 0 4.47716 0 10V156C0 161.523 4.47715 166 10 166H140C145.523 166 150 161.523 150 156V10C150 4.47715 145.523 0 140 0H10Z"
+                            />
+                        </mask>
+                        <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M10 0C4.47715 0 0 4.47716 0 10V156C0 161.523 4.47715 166 10 166H140C145.523 166 150 161.523 150 156V10C150 4.47715 145.523 0 140 0H10Z"
+                            fill="url(#pattern0)"
+                        />
+                        <path
+                            d="M0.5 10C0.5 4.7533 4.7533 0.5 10 0.5V-0.5C4.20101 -0.5 -0.5 4.20101 -0.5 10H0.5ZM0.5 156V10H-0.5V156H0.5ZM10 165.5C4.75329 165.5 0.5 161.247 0.5 156H-0.5C-0.5 161.799 4.20101 166.5 10 166.5V165.5ZM140 165.5H10V166.5H140V165.5ZM149.5 156C149.5 161.247 145.247 165.5 140 165.5V166.5C145.799 166.5 150.5 161.799 150.5 156H149.5ZM149.5 10V156H150.5V10H149.5ZM140 0.5C145.247 0.5 149.5 4.75329 149.5 10H150.5C150.5 4.20101 145.799 -0.5 140 -0.5V0.5ZM10 0.5H140V-0.5H10V0.5Z"
+                            fill="white"
+                            mask="url(#path-1-inside-1_1_40)"
+                        />
+                        <defs>
+                            <pattern
+                                id="pattern0"
+                                patternContentUnits="objectBoundingBox"
+                                width="1"
+                                height="1"
+                            >
+                                <use
+                                    xlinkHref="#image0_1_40"
+                                    transform="translate(-0.0608719) scale(0.00301544 0.0027248)"
+                                />
+                            </pattern>
+                            <image
+                                id="image0_1_40"
+                                width="372"
+                                height="367"
+                                xlinkHref="https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/7473/dc49cc3b-38ef-4b66-b3f7-eb14c947dc1b.webp"
+                            />
+                        </defs>
+                    </svg>
+                </div>
+                <p className="n-f-t-fetched-selection">Selection</p>
+                <div className="n-f-t-fetched-rectangle14" />
+                <div className="wand-magic-sparkles-solid1-wand-magic-sparkles-solid1">
+                    <svg
+                        width="100%"
+                        height="100%"
+                        preserveAspectRatio="none"
+                        viewBox="0 0 39 40"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M16.8458 0.39024C16.9203 0.155552 17.1167 9.15527e-05 17.3333 9.15527e-05C17.55 9.15527e-05 17.7464 0.155552 17.8208 0.39024L18.7755 3.33368L21.3281 4.4368C21.5312 4.52508 21.6667 4.7493 21.6667 5.00008C21.6667 5.25086 21.5312 5.47508 21.3281 5.56336L18.7755 6.66649L17.8208 9.60946C17.7464 9.84383 17.55 10.0001 17.3333 10.0001C17.1167 10.0001 16.9203 9.84383 16.8458 9.60946L15.8911 6.66649L13.2776 5.56336C13.0745 5.47508 13 5.25086 13 5.00008C13 4.7493 13.0745 4.52508 13.2776 4.4368L15.8911 3.33368L16.8458 0.39024ZM28.9385 1.09852C30.2115 -0.36601 32.2698 -0.36601 33.5359 1.09852L35.8786 3.80008C37.1448 5.26415 37.1448 7.63915 35.8786 9.10165L10.0547 38.8985C8.78854 40.3595 6.72885 40.3595 5.46 38.8985L3.11865 36.1954C1.84911 34.7345 1.84911 32.3595 3.11865 30.8907L28.9385 1.09852ZM31.2406 4.63368L24.1245 12.836L25.7021 14.6563L32.8115 6.45165L31.2406 4.63368ZM0.507203 9.15633L4.33333 7.50008L5.76807 3.08524C5.8825 2.7329 6.17432 2.50008 6.5 2.50008C6.825 2.50008 7.11615 2.7329 7.23125 3.08524L8.66667 7.50008L12.4922 9.15633C12.7969 9.28915 13 9.62508 13 10.0001C13 10.3751 12.7969 10.711 12.4922 10.8438L8.66667 12.5001L7.23125 16.9141C7.11615 17.2657 6.825 17.5001 6.5 17.5001C6.17432 17.5001 5.8825 17.2657 5.76807 16.9141L4.33333 12.5001L0.507203 10.8438C0.202109 10.711 0 10.3751 0 10.0001C0 9.62508 0.202109 9.28915 0.507203 9.15633ZM24.3411 29.1563L28.1667 27.5001L29.6021 23.086C29.7172 22.6641 30.0083 22.5001 30.3333 22.5001C30.6583 22.5001 30.9495 22.6641 31.0646 23.086L32.5 27.5001L36.3255 29.1563C36.6302 29.2891 36.8333 29.6251 36.8333 30.0001C36.8333 30.3751 36.6302 30.711 36.3255 30.8438L32.5 32.5001L31.0646 36.9141C30.9495 37.2657 30.6583 37.5001 30.3333 37.5001C30.0083 37.5001 29.7172 37.2657 29.6021 36.9141L28.1667 32.5001L24.3411 30.8438C23.9755 30.711 23.8333 30.3751 23.8333 30.0001C23.8333 29.6251 23.9755 29.2891 24.3411 29.1563Z"
+                            fill="white"
+                        />
+                    </svg>
+                </div>
+                <p className="n-f-t-fetched-please-select-an-nft">
+                    Please select an NFT to display and edit its metadata
+                </p>
+                <div className="blob2-blob2">
+                    <svg
+                        width="100%"
+                        height="100%"
+                        preserveAspectRatio="none"
+                        viewBox="0 0 1031 592"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <g clipPath="url(#clip0_1_27)" filter="url(#filter0_f_1_27)">
+                            <path
+                                d="M517.464 -36.5465C574.102 -21.1495 640.832 -16.7228 666.174 14.2395C691.516 45.2018 675.471 102.7 652.749 141.822C630.486 181.084 601.6 202.448 560.512 211.58C519.679 221.02 467.155 218.845 414.371 184.135C361.588 149.424 309.004 82.3175 301.296 28.409C293.385 -25.3311 330.606 -65.5651 372.415 -72.1761C414.172 -79.2634 460.772 -52.4198 517.464 -36.5465Z"
+                                fill="#02DC85"
+                                fillOpacity="0.8"
+                            />
+                        </g>
+                        <defs>
+                            <filter
+                                id="filter0_f_1_27"
+                                x="-44"
+                                y="-489"
+                                width="1074.04"
+                                height="1080.62"
+                                filterUnits="userSpaceOnUse"
+                                colorInterpolationFilters="sRGB"
+                            >
+                                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                                <feBlend
+                                    mode="normal"
+                                    in="SourceGraphic"
+                                    in2="BackgroundImageFix"
+                                    result="shape"
+                                />
+                                <feGaussianBlur
+                                    stdDeviation="150"
+                                    result="effect1_foregroundBlur_1_27"
+                                />
+                            </filter>
+                            <clipPath id="clip0_1_27">
+                                <rect
+                                    width="313.878"
+                                    height="364.093"
+                                    fill="white"
+                                    transform="translate(256 11.4001) rotate(-39.6774)"
+                                />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+
+
+
+
+
+
+            </div>
+            <div className="blob3-blob3">
+                <svg
+                    width="100%"
+                    height="100%"
+                    preserveAspectRatio="none"
+                    viewBox="0 0 748 773"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <g clipPath="url(#clip0_1_23)" filter="url(#filter0_f_1_23)">
+                        <path
+                            d="M172.779 349.975C245.776 370.617 331.591 377.242 364.583 417.538C397.574 457.834 377.741 531.801 349.077 581.967C321.005 632.321 284.176 659.489 231.498 670.709C179.153 682.33 111.623 678.805 43.3161 633.297C-24.9913 587.789 -93.4858 500.485 -104.129 430.824C-115.031 361.378 -67.7491 309.975 -14.1106 302.018C39.4539 293.445 99.7074 328.718 172.779 349.975Z"
+                            fill="#EB54BC"
+                            fillOpacity="0.8"
+                        />
+                    </g>
+                    <defs>
+                        <filter
+                            id="filter0_f_1_23"
+                            x="-462.572"
+                            y="-147"
+                            width="1210.28"
+                            height="1220"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="BackgroundImageFix"
+                                result="shape"
+                            />
+                            <feGaussianBlur
+                                stdDeviation="150"
+                                result="effect1_foregroundBlur_1_23"
+                            />
+                        </filter>
+                        <clipPath id="clip0_1_23">
+                            <rect
+                                width="399.8"
+                                height="473.905"
+                                fill="white"
+                                transform="translate(-162.572 408.258) rotate(-39.6774)"
+                            />
+                        </clipPath>
+                    </defs>
+                </svg>
+            </div>
+
+
+        </div>
+
+    )
+}
---
+++ pages/mutate.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import NFTFetched from '../components/layout/NFTFetched'
+
+const Mutate: NextPage = () => {
+  return (
+    <>
+    <NFTFetched />
+    </>
+  )
+}
+
+export default Mutate
